afa9e0ca7ca5a6ebb0280d08e824ce9c5f44a604
YangSpring114 <blessing_114514@qq.com>
2023-12-30 14:37:49 +0800

    修复部分404页面

--- docs/.vitepress/navBar/zhCN.ts
+++ docs/.vitepress/navBar/zhCN.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "vitepress";
 
 export const nav: DefaultTheme.NavItem[] = [
     { text: '首页', link: '/' },
-    { text: 'MinecraftLaunch', link: '/' },
+    { text: 'MinecraftLaunch', link: '/zhCN/minecraftlaunch/' },
     { text: "我们的团队", link: '/zhCN/introduce' }
 ];
 
--- docs/.vitepress/sidebar/zhCN.ts
+++ docs/.vitepress/sidebar/zhCN.ts
@@ -10,10 +10,10 @@ export const sidebar: DefaultTheme.SidebarItem[] = [
         ]
     },
     {
-        text: "MinecraftLaunch",
+        text: "minecraftlaunch",
         collapsed: false,
         items: [
-            {text: "简介", link: "/zhCN/minecraftLaunch/"},
+            {text: "简介", link: "/zhCN/minecraftlaunch/"},
             { text: '安装', link: '/zhCN/minecraftlaunch/install' },
             { text: '启动器', link: '/zhCN/minecraftlaunch/launcher' },
             {
@@ -27,35 +27,35 @@ export const sidebar: DefaultTheme.SidebarItem[] = [
             {
                 text: "验证器",
                 collapsed: false,
-                link: "/zhCN/minecraftLaunch/authenticators/",
+                link: "/zhCN/minecraftlaunch/authenticators/",
                 items:[
-                    {text: "Offline 验证器", link: "/zhCN/minecraftLaunch/authenticators/offline"},
-                    {text: "Yggdrasil 验证器", link: "/zhCN/minecraftLaunch/authenticators/yggdrasil"},
-                    {text: "Microsoft 验证器", link: "/zhCN/minecraftLaunch/authenticators/microsoft"}
+                    {text: "Offline 验证器", link: "/zhCN/minecraftlaunch/authenticators/offline"},
+                    {text: "Yggdrasil 验证器", link: "/zhCN/minecraftlaunch/authenticators/yggdrasil"},
+                    {text: "Microsoft 验证器", link: "/zhCN/minecraftlaunch/authenticators/microsoft"}
                 ]
             },
             {
                 text: "安装器",
                 collapsed: false,
-                link: "/zhCN/minecraftLaunch/installers/",
+                link: "/zhCN/minecraftlaunch/installers/",
                 items: [
-                    {text: "Fabric 安装器", link: "/zhCN/minecraftLaunch/installers/fabric"},
-                    {text: "Forge 安装器", link: "/zhCN/minecraftLaunch/installers/forge"},
-                    {text: "Optifine 安装器", link: "/zhCN/minecraftLaunch/installers/optifine"},
-                    {text: "Quilt 安装器", link: "/zhCN/minecraftLaunch/installers/quilt"}
+                    {text: "Fabric 安装器", link: "/zhCN/minecraftlaunch/installers/fabric"},
+                    {text: "Forge 安装器", link: "/zhCN/minecraftlaunch/installers/forge"},
+                    {text: "Optifine 安装器", link: "/zhCN/minecraftlaunch/installers/optifine"},
+                    {text: "Quilt 安装器", link: "/zhCN/minecraftlaunch/installers/quilt"}
                 ]
             },
             {
                 text: "检查器",
                 collapsed: false,
-                link: "/zhCN/minecraftLaunch/checks/",
+                link: "/zhCN/minecraftlaunch/checks/",
                 items: [
                 ]
             },
             {
                 text: "搜寻器",
                 collapsed: false,
-                link: "/zhCN/minecraftLaunch/fetcher/",
+                link: "/zhCN/minecraftlaunch/fetcher/",
                 items: [
                 ]
             },
